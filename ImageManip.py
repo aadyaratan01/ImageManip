ce36ec8ab5234ce4788b15e860ed6af8016d29ea
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-10-18 15:39:35 +0530

    Create ImageManip.py

---
+++ ImageManip.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+
+def alter_brightness(pixel, level):
+    """Alter RGB brightness level
+    This is equal to multiplying each row in the matrix by level
+    """
+    pixel = [int(component * level) for component in pixel]
+
+    return tuple(pixel)
+
+
+image = Image.open("/home/lalit/Pictures/Background.jpg").convert("RGB")
+
+# Get a list of tuples representing each pixel [(Red, Green, Blue), (Red, Green, Blue), ...]
+image_data = image.getdata()
+
+# Modify each pixel's RGB value
+image_data_modified = [alter_brightness(pixel, 1.15) for pixel in image_data]
+
+# Create a new temporary image
+new_image = Image.new(mode="RGB", size=image.size)
+
+# Copy the modified pixel data onto new image
+new_image.putdata(image_data_modified)
+
+# Show the newly generated image
+new_image.show()
