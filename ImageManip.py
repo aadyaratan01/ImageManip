90195b7355edcfe0e226452946876dfdd9549b9f
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2021-10-19 15:07:48 +0530

    Update ImageManip.py

--- ImageManip.py
+++ ImageManip.py
@@ -1,43 +1,68 @@
 from PIL import Image
 
 
-def alter_brightness(pixel, level):
-    """Alter RGB brightness level
-    This is equal to multiplying each row in the matrix by level
+def increase_brightness(image: Image):
+    """Increase brightness by 10%
+    This is equal to multiplying each row in the matrix by 1.10
     """
-    pixel = [int(component * level) for component in pixel]
 
-    return tuple(pixel)
+    # Get a list of tuples representing each pixel [(Red, Green, Blue), (Red, Green, Blue), ...]
+    image_data = image.getdata()
 
-def skew_image(image):
-    return image.transform(image.size, Image.AFFINE, (1, -0.5, 0.5 * image.size[0], 0, 1, 0))
+    modified_image_data = []
 
-def crop_and_resize(image):
-    return image.transform(image.size, Image.EXTENT, (30, 40, image.size[0] * 5 // 6, image.size[1] // 2 + 40))
+    # Modify each component (Red, Green, and Blue) in every pixel
+    for pixel in image_data:
+        pixel = [int(component * 1.10) for component in pixel]
 
-def make_negative(pixel):
-	''' Make each RGB pixel negative (in color)
+        modified_image_data.append(tuple(pixel))
 
-	This is equal to subtracting two matrices
-	'''
-	pixel = [255 - component for component in pixel]
+    modified_image = Image.new(mode="RGB", size=image.size)
 
-	return tuple(pixel)
+    modified_image.putdata(modified_image_data)
+
+    return modified_image
+
+
+def make_negative(image: Image):
+    """Make image negative in color
+    This is equal to subtracting two matrices [(Red, Green, Blue), ...] from [(255, 255, 255), ...]
+    """
+
+    # Get a list of tuples representing each pixel [(Red, Green, Blue), (Red, Green, Blue), ...]
+    image_data = image.getdata()
+
+    modified_image_data = []
+
+    # Modify each component (Red, Green, and Blue) in every pixel
+    for pixel in image_data:
+        pixel = [255 - component for component in pixel]
+
+        modified_image_data.append(tuple(pixel))
+
+    modified_image = Image.new(mode="RGB", size=image.size)
+
+    modified_image.putdata(modified_image_data)
+
+    return modified_image
+
+
+def invalid_choice(image: Image):
+    print("Invalid choice, source image will be shown")
+
+    return image
 
 
 image = Image.open(input("Enter image path: ")).convert("RGB")
 
-# Get a list of tuples representing each pixel [(Red, Green, Blue), (Red, Green, Blue), ...]
-image_data = image.getdata()
+choices = {1: increase_brightness, 2: make_negative, 3: skew_image, 4: crop_and_resize}
 
-# Modify each pixel's RGB value
-image_data_modified = [alter_brightness(pixel, 1.15) for pixel in image_data]
+choice = int(
+    input(
+        "Enter:\n1. To increase brightness by 10%\n2. To make image negative in color\n3. To skew image\n 4. To crop and resize image\n:"
+    )
+)
 
-# Create a new temporary image
-new_image = Image.new(mode="RGB", size=image.size)
+image = choices.get(choice, invalid_choice)(image)
 
-# Copy the modified pixel data onto new image
-new_image.putdata(image_data_modified)
-
-# Show the newly generated image
-new_image.show()
+image.show()
